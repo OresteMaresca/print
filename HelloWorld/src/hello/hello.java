f3bb6cd946ae8e6aeb781a359ff25c38f80b374a
Oreste Maresca <oreste.a.maresca@gmail.com>
2017-08-16 13:24:27 -0300

    Second Commit

--- HelloWorld/src/hello/hello.java
+++ HelloWorld/src/hello/hello.java
@@ -4,5 +4,7 @@ public class hello {
 
 	public static void main(String[] args){
 		System.out.println("Hello world!");
+		// This is my second commit
+		System.out.println("Greetings from Uruguay");
 	}
 }
